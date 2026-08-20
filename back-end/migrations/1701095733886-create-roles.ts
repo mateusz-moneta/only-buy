93f353386c8825d00789caa0c6de8f616207d5f1
Mateusz Moneta <mateusz.moneta@akmf.pl>
2026-08-20 15:32:36 +0200

    Fix for migrations

--- back-end/migrations/1701095733886-create-roles.ts
+++ back-end/migrations/1701095733886-create-roles.ts
@@ -1,13 +1,20 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateRoles1701095733886 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TABLE "roles" (
+        "id" SERIAL NOT NULL,
+        "name" VARCHAR NOT NULL,
+        CONSTRAINT "PK_roles" PRIMARY KEY ("id"),
+        CONSTRAINT "UQ_roles_name" UNIQUE ("name")
+      )
+    `);
+    await queryRunner.query(`INSERT INTO roles (name) VALUES ('STANDARD')`);
+    await queryRunner.query(`INSERT INTO roles (name) VALUES ('ADMIN')`);
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`INSERT INTO roles (name) VALUES ('STANDARD')`);
-        await queryRunner.query(`INSERT INTO roles (name) VALUES ('ADMIN')`);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "roles"`);
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "roles"`);
+  }
 }
--- back-end/migrations/1701097096009-create-user.ts
+++ back-end/migrations/1701097096009-create-user.ts
@@ -1,8 +1,24 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUser1701097096009 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE "users" (
+                                     "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+                                     "username" VARCHAR NOT NULL,
+                                     "email" VARCHAR NOT NULL,
+                                     "password" VARCHAR NOT NULL,
+                                     "createdDate" TIMESTAMP NOT NULL DEFAULT now(),
+                                     "updatedDate" TIMESTAMP NOT NULL DEFAULT now(),
+                                     "roleId" INTEGER NOT NULL,
+                                     "active" BOOLEAN NOT NULL DEFAULT true,
+                                     "avatar" VARCHAR,
+                                     CONSTRAINT "PK_users" PRIMARY KEY ("id"),
+                                     CONSTRAINT "UQ_users_username" UNIQUE ("username"),
+                                     CONSTRAINT "UQ_users_email" UNIQUE ("email")
+            )
+        `);
+    await queryRunner.query(`
       INSERT INTO users (
         "id",
         "username",
@@ -11,6 +27,7 @@ export class CreateUser1701097096009 implements MigrationInterface {
         "createdDate",
         "updatedDate",
         "roleId",
+        "active",
         "avatar"
       )
       VALUES (
@@ -21,15 +38,16 @@ export class CreateUser1701097096009 implements MigrationInterface {
         '2024-02-14 19:13:50.909089',
         '2024-02-14 19:13:50.909089',
         2,
+              true,
         NULL
       )
     `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
       DELETE FROM users
       WHERE id = '9a111626-d24b-467c-b2c9-a2fcabf07ddf'
     `);
-    }
+  }
 }
